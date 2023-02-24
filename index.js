cc69778f65a851b3ac955d698efd5f9965f6e43f
Vrushank796 <42446203+Vrushank796@users.noreply.github.com>
2023-02-24 17:03:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,16 @@ newspapers.forEach((newspaper) => {
 
 //Display message on home
 app.get('/', (req, res) => {
-  res.json('Welcome to my Climate Change News API');
+  res.send('<h1> Welcome to my Climate Change News API \nAn API showing all the latest Climate Change News around the world.</h1>
+
+<div>To explore all news channels: https://climate-change-api-3fd7.onrender.com/news</div>
+
+<div>To explore The Times: https://climate-change-api-3fd7.onrender.com/news/thetimes</div> 
+
+<div>To explore The Guardian: https://climate-change-api-3fd7.onrender.com/news/theguardian</div> 
+
+<div>To explore The Telegraph: https://climate-change-api-3fd7.onrender.com/news/thetelegraph</div> 
+');
 });
 
 //Fetch the muliple news sources
