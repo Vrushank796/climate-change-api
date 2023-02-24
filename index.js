66e423d4131d2c2a7be307be098f9bcc5cd75bf6
Vrushank796 <42446203+Vrushank796@users.noreply.github.com>
2023-02-24 17:14:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -56,10 +56,10 @@ app.get('/', (req, res) => {
   res.json({
   "greeting": "Welcome to my Climate Change News API \\nAn API showing all the latest Climate Change News around the world.",
   "links": [
-    {"To explore all news channels": "https://climate-change-api-3fd7.onrender.com/news"},
-     { "To explore The Times": "https://climate-change-api-3fd7.onrender.com/news/thetimes"},
-    {"To explore The Guardian": "https://climate-change-api-3fd7.onrender.com/news/theguardian"},
-    {"To explore The Telegraph": "https://climate-change-api-3fd7.onrender.com/news/thetelegraph"},
+    {"All news channels": "https://climate-change-api-3fd7.onrender.com/news"},
+     { "The Times": "https://climate-change-api-3fd7.onrender.com/news/thetimes"},
+    {"The Guardian": "https://climate-change-api-3fd7.onrender.com/news/theguardian"},
+    {"The Telegraph": "https://climate-change-api-3fd7.onrender.com/news/thetelegraph"},
   ]
 });
 });
